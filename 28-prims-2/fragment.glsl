d2ef4db0d01378f2b555f6d2af2832794f6982b8
Armand Biteau <armand.biteau@mediamonks.com>
2016-12-08 15:09:02 +0000

    frontFacing

--- 28-prims-2/fragment.glsl
+++ 28-prims-2/fragment.glsl
@@ -3,5 +3,15 @@ precision highp float;
 uniform vec3 frontColor, backColor;
 
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+    vec3 color = vec3(0.0);
+
+    if(gl_FrontFacing){
+        color = frontColor;
+    } else {
+        color = backColor;
+    }
+
+  gl_FragColor = vec4(color, 1.0);
+
 }
