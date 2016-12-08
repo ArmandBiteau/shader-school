02fee7c8eeedd77c7ad46de9301e70a8ea10479d
Armand Biteau <armand.biteau@mediamonks.com>
2016-12-08 14:17:32 +0000

    try pointLight

--- 20-light-4/fragment.glsl
+++ 20-light-4/fragment.glsl
@@ -16,6 +16,42 @@ uniform vec3 lightPosition;
 
 uniform float shininess;
 
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying vec3 vLightDirection;
+
+float phongWeight(
+    vec3 lightDirection,
+    vec3 surfaceNormal,
+    vec3 eyeDirection,
+    float shininess
+) {
+    //First reflect light by surface normal
+    vec3 rlight = reflect(lightDirection, surfaceNormal);
+
+    //Next find the projected length of the reflected
+    //light vector in the view direction
+    float eyeLight = max(dot(rlight, eyeDirection), 0.0);
+
+    //Finally exponentiate by the shininess factor
+    return pow(eyeLight, shininess);
+}
+
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+
+    vec3 eyeDirection = normalize(vPosition);
+
+    vec3 normal = normalize(vNormal);
+
+    vec3 light = normalize(vLightDirection);
+
+    float lambert = dot(normal, light);
+
+    float phong = phongWeight(light, normal, eyeDirection, shininess);
+
+    vec3 color = ambient + diffuse * lambert + specular * phong;
+
+
+    gl_FragColor = vec4(color, 0.5);
+
 }
--- 20-light-4/vertex.glsl
+++ 20-light-4/vertex.glsl
@@ -13,6 +13,23 @@ uniform mat4 inverseProjection;
 
 uniform vec3 lightPosition;
 
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying vec3 vLightDirection;
+
+
 void main() {
-  gl_Position = vec4(position, 1);
+
+    vec4 viewPosition = view * model * vec4(position, 1.0);
+    vec4 viewNormal = vec4(normal, 0.0) * inverseModel * inverseView;
+
+    vec4 viewLight = view * vec4(lightPosition, 1.0);
+
+    gl_Position = projection * viewPosition;
+
+    vNormal = viewNormal.xyz;
+    vPosition = viewPosition.xyz;
+
+    vLightDirection = viewLight.xyz - viewPosition.xyz;
+
 }
