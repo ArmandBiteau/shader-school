37e044387421c049dd37803845706b986bda3991
Armand Biteau <armand.biteau@mediamonks.com>
2016-12-12 13:53:35 +0000

    heat

--- 25-gpgpu-2/heat.glsl
+++ 25-gpgpu-2/heat.glsl
@@ -9,13 +9,18 @@ float state(vec2 x) {
   return texture2D(prevState, fract(x / stateSize)).r;
 }
 
+float laplacian(vec2 x) {
+
+  return (state( x+vec2(-1,0) ) + state( x+vec2(1,0) ) + state( x+vec2(0,1) ) + state( x+vec2(0,-1) )) - 4.0 * state(x);
+
+}
+
 void main() {
-  vec2 coord = gl_FragCoord.xy;
 
-  //TODO: Compute next state using a 5-point Laplacian stencil and the rule
+    vec2 coord = gl_FragCoord.xy;
 
-  float y = state(coord);
+    float y = (1.0 - kdamping) * (kdiffuse * laplacian(coord) + state(coord));
 
+    gl_FragColor = vec4(y,y,y,y);
 
-  gl_FragColor = vec4(y,y,y,1);
 }
